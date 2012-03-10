495eee2e2784799caea656722160fb342e38446a
cjwabcd <qq250188850@gmail.com>
2012-03-10 11:09:16 -0600

    0.0.1

---
+++ Baitan/src/com/baitan001/BaitanActivity.java
@@ -0,0 +1,13 @@
+package com.baitan001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaitanActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
