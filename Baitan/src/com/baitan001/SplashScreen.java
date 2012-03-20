ebd57590a0812ac6232dccce78cd8e0becc12a7a
cjwabcd <qq250188850@gmail.com>
2012-03-19 23:18:29 -0500

    Splash Screen

---
+++ Baitan/src/com/baitan001/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.baitan001;
+ 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+ 
+public class SplashScreen extends Activity {
+   /** Called when the activity is first created. */
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.splash);
+
+      
+      Thread splashThread = new Thread() {
+         @Override
+         public void run() {
+            try {
+               int waited = 0;
+               while (waited < 5000) {
+                  sleep(100);
+                  waited += 100;
+               }
+            } catch (InterruptedException e) {
+               // do nothing
+            } finally {
+               finish();
+               Intent i = new Intent();
+               i.setClassName("com.baitan001",
+                              "com.baitan001.BaitanActivity");
+               startActivity(i);
+            }
+         }
+      };
+      splashThread.start();
+   }
+}
