e5698bd8d0663b030caa9830d5b2dbcda3f6c2f5
cjwabcd <qq250188850@gmail.com>
2012-07-02 19:20:44 +0800

    connect with zxing

---
+++ Baitan/src/com/baitan001/android/CurrentLocation.java
@@ -0,0 +1,18 @@
+package com.baitan001.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CurrentLocation extends Activity{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.location);
+
+		
+		
+		
+	}
+
+}
